e229ff638bed3a7dd58a323913c9269a57941f41
Valter Wiklund <valterwiklund@Mac.lan>
2026-09-10 14:52:00 +0200

    Update

--- src/components/InfoGrid.tsx
+++ src/components/InfoGrid.tsx
@@ -20,7 +20,7 @@ const cards: InfoCard[] = [
   {
     icon: Trophy,
     title: 'Vinnaren tar allt',
-    description: '5000 kr i garanterad prispott.',
+    description: '5000 kr + Exklusiva priser från EF i prispott.',
   },
 ]
 
